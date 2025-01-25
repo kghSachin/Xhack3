74f7cbdef7ac17c8fd933911d724703c599cee64
sachin <118202383+kghSachin@users.noreply.github.com>
2025-01-25 15:25:08 +0545

    fix branch

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,20 +7,14 @@ import Foundation
 
 import geolocator_apple
 import location
-<<<<<<< HEAD
 import path_provider_foundation
-import sqflite_darwin
-=======
 import shared_preferences_foundation
->>>>>>> 240b9c6c7b8b5cff9afb60dc9bae9e28ddfa2002
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
-<<<<<<< HEAD
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-=======
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
->>>>>>> 240b9c6c7b8b5cff9afb60dc9bae9e28ddfa2002
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
